6e4284bce39bb4e64b19b9ed1cfffa09089df8ce
Ultra119 <pasasvistak0@gmail.com>
2023-05-20 01:03:42 +0300

    Add currency ratio
    - Also refactor and improve readability

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import requests
 from pyrogram.types import Message
-from base.module import BaseModule
-from base.module import command
 from pyrogram import Client
+from base.module import BaseModule, command
 
 class CurrencyModule(BaseModule):
+    API_URL = "https://api.exchangerate-api.com/v4/latest/"
+
     @command("currency")
     def currency_command(self, client: Client, message: Message):
         command_parts = message.text.split(" ")
+
         if len(command_parts) != 4:
             message.reply(self.S["usage"])
             return
 
-        number = command_parts[1]
-        source_currency = command_parts[2].upper()
-        target_currency = command_parts[3].upper()
-
+        number, source_currency, target_currency = command_parts[1].upper(), command_parts[2].upper(), command_parts[3].upper()
         try:
-            response = requests.get(f"https://api.exchangerate-api.com/v4/latest/{source_currency}")
+            response = requests.get(f"{self.API_URL}/{source_currency}")
             data = response.json()
 
             if source_currency not in data["rates"] or target_currency not in data["rates"]:
                 message.reply(self.S["invalid_currency"])
                 return
 
+            ratio = data["rates"][target_currency] / data["rates"][source_currency]
             rate = data["rates"][target_currency]
             result = float(number) * rate
 
-            message.reply(f"{number} {source_currency} = {result} {target_currency}")
+            message.reply(f"1 {source_currency} = {ratio} {target_currency}\n{number} {source_currency} = {result} {target_currency}")
 
         except Exception as e:
+            self.logger.error(e)
             message.reply(self.S["error"])
