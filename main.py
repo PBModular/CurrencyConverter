587c6bf81be9097fafebacfa6e17f77b95a2e11c
Ultra119 <pasasvistak0@gmail.com>
2023-05-20 00:47:54 +0300

    Add translations

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class CurrencyModule(BaseModule):
     def currency_command(self, client: Client, message: Message):
         command_parts = message.text.split(" ")
         if len(command_parts) != 4:
-            message.reply("Использование: /currency <number> <source_currency> <target_currency>")
+            message.reply(self.S["usage"])
             return
 
         number = command_parts[1]
@@ -21,7 +21,7 @@ class CurrencyModule(BaseModule):
             data = response.json()
 
             if source_currency not in data["rates"] or target_currency not in data["rates"]:
-                message.reply("Неверно указаны валюты.")
+                message.reply(self.S["invalid_currency"])
                 return
 
             rate = data["rates"][target_currency]
@@ -30,4 +30,4 @@ class CurrencyModule(BaseModule):
             message.reply(f"{number} {source_currency} = {result} {target_currency}")
 
         except Exception as e:
-            message.reply("Произошла ошибка при выполнении команды.")
+            message.reply(self.S["error"])
