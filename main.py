c7dade23c80664635dde4382fdc5a3d3895a10ba
Ultra119 <pasasvistak0@gmail.com>
2023-06-12 11:43:40 +0300

    main: Add unicode flags support

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from pyrogram.types import Message
 from pyrogram import Client
@@ -5,6 +6,16 @@ from base.module import BaseModule, command
 
 class CurrencyModule(BaseModule):
     API_URL = "https://api.exchangerate-api.com/v4/latest/"
+    FLAG_CODES = {}
+    
+    def on_init(self):
+        try:
+            with open("flags.json", "r") as file:
+                data = json.load(file)
+                self.FLAG_CODES = data.get("flag_codes", {})
+            
+        except Exception as e:
+            self.logger.error(e)
 
     @command("currency")
     def currency_command(self, client: Client, message: Message):
@@ -15,11 +26,7 @@ class CurrencyModule(BaseModule):
             return
 
         number, source_currency = command_parts[1].upper(), command_parts[2].upper()
-        target_currencies = ["USD", "EUR", "GBP", "JPY", "CAD"]
-
-        if len(command_parts) > 3:
-            target_currency = command_parts[3].upper()
-            target_currencies = [target_currency]
+        target_currency = command_parts[3].upper() if len(command_parts) > 3 else None
 
         try:
             response = requests.get(f"{self.API_URL}/{source_currency}")
@@ -31,16 +38,28 @@ class CurrencyModule(BaseModule):
 
             result_message = ""
 
-            for target_currency in target_currencies:
-                if target_currency == source_currency:
+            target_currencies = [target_currency] if target_currency else ["USD", "EUR", "GBP", "JPY", "CAD"]
+
+            for target in target_currencies:
+                if target == source_currency:
+                    if not target_currency:
+                        continue
+                    
                     message.reply(self.S["same_currency"])
                     return
-                
-                ratio = data["rates"][target_currency] / data["rates"][source_currency]
-                rate = data["rates"][target_currency]
+
+                ratio = data["rates"][target] / data["rates"][source_currency]
+                rate = data["rates"][target]
                 result = round(float(number) * rate, 2)
 
-                result_message += f"1 {source_currency} = {ratio} {target_currency}\n{number} {source_currency} = {result} {target_currency}\n\n"
+                source_flag = self.FLAG_CODES.get(source_currency, "")
+                target_flag = self.FLAG_CODES.get(target, "")
+
+                result_message += (
+                    f"{source_flag} ⇆ {target_flag}\n"
+                    f"1 {source_currency} = {ratio} {target}\n"
+                    f"{number} {source_currency} = {result} {target}\n\n"
+                )
 
             message.reply(result_message)
 
